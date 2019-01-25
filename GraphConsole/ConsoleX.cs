9ff808b1ce99841f045275e948145d5384f08902
Unknown <dmi3dmi3@mail.ru>
2019-01-25 06:29:52 +0300

    Чистый алгоритм с простым консольным интерфейсом

---
+++ GraphConsole/ConsoleX.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace GraphConsole
+{
+    public class ConsoleX
+    {
+        public static void Write(string data, ConsoleColor color)
+        {
+            var backColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(data);
+            Console.ForegroundColor = backColor;
+        }
+
+        public static void WriteLine(string data, ConsoleColor color)
+        {
+            Write(data + Environment.NewLine, color);
+        }
+
+        public static void PrintValue(string name, object item)
+        {
+
+            Console.Write($"{name}:");
+            switch (item)
+            {
+                case bool boolValue:
+                    WriteLine(boolValue.ToString(), boolValue ? ConsoleColor.Green : ConsoleColor.Red);
+                    break;
+                case string stringValue:
+                    WriteLine(stringValue, ConsoleColor.Cyan);
+                    break;
+                case int intValue:
+                    WriteLine(intValue.ToString(), ConsoleColor.Yellow);
+                    break;
+                case float intValue:
+                    WriteLine(intValue.ToString(), ConsoleColor.Yellow);
+                    break;
+                case decimal intValue:
+                    WriteLine(intValue.ToString(), ConsoleColor.Yellow);
+                    break;
+                case long intValue:
+                    WriteLine(intValue.ToString(), ConsoleColor.Yellow);
+                    break;
+                case Enum enumValue:
+                    WriteLine(enumValue.ToString(),ConsoleColor.Magenta);
+                    break;
+                case null:
+                    WriteLine("NULL", ConsoleColor.Red);
+                    break;
+                default:
+                    WriteLine(item.ToString(), ConsoleColor.White);
+                    break;
+            }
+
+        }
+
+        public static void PrintBytes(string name, long bytes)
+        {
+            var kbytes = bytes / 1024D;
+
+            var mbytes = bytes / (1024D * 1024D);
+
+
+            Console.Write($"{name}:");
+            Write(bytes.ToString(), ConsoleColor.Cyan);
+            Write("b | ", ConsoleColor.Gray);
+            Write(kbytes.ToString("F"), ConsoleColor.Cyan);
+            Write("kb | ", ConsoleColor.Gray);
+            Write(mbytes.ToString("F"), ConsoleColor.Cyan);
+            WriteLine("mb | ", ConsoleColor.Gray);
+        }
+
+        public static TItem GetValue<TItem>(string name)
+        {
+            while (true)
+            {
+                Console.Write($"{name}: ");
+                var stringItem = Console.ReadLine();
+                var itemType = typeof(TItem);
+                
+                if (itemType == typeof(int))
+                {
+                    if (int.TryParse(stringItem, out var intResult))
+                        return (TItem)Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(long))
+                {
+                    if (long.TryParse(stringItem, out var intResult))
+                        return (TItem)Convert.ChangeType(intResult, typeof(TItem));
+                }
+                if (itemType == typeof(float))
+                {
+                    if (float.TryParse(stringItem, out var intResult))
+                        return (TItem)Convert.ChangeType(intResult, typeof(TItem));
+                }
+                if (itemType == typeof(double))
+                {
+                    if (double.TryParse(stringItem, out var intResult))
+                        return (TItem)Convert.ChangeType(intResult, typeof(TItem));
+                }
+                if (itemType == typeof(string))
+                    return (TItem)Convert.ChangeType(stringItem, typeof(TItem));
+
+                Console.Write("Invalid data, try again");
+                WriteLine($"({typeof(TItem)}).", ConsoleColor.DarkGray);
+            }
+
+        }
+
+        public static TItem GetValue<TItem>(string name, TItem defaultValue)
+        {
+            while (true)
+            {
+                Console.Write($"{name}[{defaultValue}]: ");
+                var stringItem = Console.ReadLine();
+                if (string.IsNullOrEmpty(stringItem))
+                    return defaultValue;
+
+                var itemType = typeof(TItem);
+
+                if (itemType == typeof(int))
+                {
+                    if (int.TryParse(stringItem, out var intResult))
+                        return (TItem) Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(long))
+                {
+                    if (long.TryParse(stringItem, out var intResult))
+                        return (TItem) Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(float))
+                {
+                    if (float.TryParse(stringItem, out var intResult))
+                        return (TItem) Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(double))
+                {
+                    if (double.TryParse(stringItem, out var intResult))
+                        return (TItem) Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(DateTime))
+                {
+                    if (DateTime.TryParse(stringItem, out var intResult))
+                        return (TItem) Convert.ChangeType(intResult, typeof(TItem));
+                }
+
+                if (itemType == typeof(Enum))
+                {
+                    try
+                    {
+                        var enumValue = Enum.Parse(typeof(TItem), stringItem, true);
+                        return (TItem)Convert.ChangeType(enumValue, typeof(TItem));
+                    }
+                    catch (Exception e)
+                    {
+                
+                    }
+                
+                }
+            
+
+                if (itemType == typeof(string))
+                    return (TItem) Convert.ChangeType(stringItem, typeof(TItem));
+               }
+        }
+
+    }
+}
---
+++ GraphConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphTools;
+
+namespace GraphConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                ConsoleX.WriteLine("Введите граф (0 - список смежности, 1 - список дуг, 2 - выход)", ConsoleColor.White);
+                var i = ConsoleX.GetValue<int>("Тип ввода", 1);
+                GraphModel graph = null;
+                if (i == 0)
+                {
+                    var nv = ConsoleX.GetValue<int>("Кол-во вершин");
+                    var dict = new Dictionary<int, List<int>>();
+                    for (int j = 0; j < nv; j++)
+                    {
+                        dict.Add(j, new List<int>());
+                        var nv2 = ConsoleX.GetValue<int>($"Кол-во смежных вершин c {j}");
+                        for (int k = 0; k < nv2; k++)
+                        {
+                            dict[j].Add(ConsoleX.GetValue<int>($"{k} вершина"));
+                        }
+                    }
+                    graph = new GraphModel(nv, dict);
+                }
+                else if (i == 1)
+                {
+                    var nv = ConsoleX.GetValue<int>("Кол-во вершин");
+                    var ne = ConsoleX.GetValue<int>("Кол-во дуг");
+                    var edges = new List<(int,int)>();
+                    for (int j = 0; j < ne; j++)
+                    {
+                        var f = ConsoleX.GetValue<int>($"Первый конец {j} дуги");
+                        var l = ConsoleX.GetValue<int>($"Второй конец {j} дуги");
+                        edges.Add((f,l));
+                    }
+                    graph = new GraphModel(nv, edges);
+                }
+                else if (i == 2)
+                {
+                    break;
+                }
+
+                var facets = FacetTools.GetFacets(graph);
+                for (int j = 0; j < facets.Count; j++)
+                {
+                    ConsoleX.Write($"{j} поверхность: ", ConsoleColor.White);
+                    foreach (var item in facets[j])
+                    {
+                        ConsoleX.Write($"{item} ", ConsoleColor.White);
+                    }
+                    ConsoleX.WriteLine("",ConsoleColor.Black);
+                }
+            } while (true);
+
+        }
+    }
+}
